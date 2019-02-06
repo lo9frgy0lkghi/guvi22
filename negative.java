90e8b362c2c5b1f128f603ef4d880ea6b0cc4712
lo9frgy0lkghi <47377205+lo9frgy0lkghi@users.noreply.github.com>
2019-02-06 15:05:21 +0530

    Create negative.java

---
+++ negative.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class ideone
+{
+	public static void main(String[]args)
+	{
+		int a;
+		Scanner S=new Scanner(System.in);
+		System.out.println("");
+		a=S.nextInt();
+		if(a>0)
+		{
+			System.out.println("Positive");
+		}
+		else if(a==0)
+		{
+			System.out.println("Zero");
+		}
+		else
+		{
+			System.out.println("Negative");
+		}
+	}
+	
+}	
+	
